daa8b6dfba52f1f42f71d1fb51502f54ff412eed
sarwdnya <sarwdnya@DESKTOP-GJR80O2>
2022-10-08 15:49:13 +0530

    [2nd Changes

--- src/Sample1/Demo.java
+++ src/Sample1/Demo.java
@@ -1,5 +1,12 @@
 package Sample1;
 
-public class Demo {
+public class Demo
+{ 
+	public static void main(String[] args)
+	{
+		System.out.println("Hiii");
+		System.out.println("Hello");
+		
+	}
 
 }
